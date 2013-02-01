6033fff594d10a279b49cf500bc298aadff0fbe8
Qianze Zhang <qianze.zhang@gmail.com>
2013-02-01 17:16:41 +0000

    text msg supported

--- public/js/common.js
+++ public/js/common.js
@@ -12,6 +12,8 @@ var common = {
       common.dom.msgBoxWrapper = $('#msg-box-wrapper');
       var navLinks = $('#navigation .nav-link');
       var msgBox = $('#msg-box-wrapper');
+      var msgInput = $('#say-something');
+      common.prepareMessageInput(msgInput);
       common.appear(navLinks, msgBox);
       common.dynamicMessage();
     },
@@ -22,15 +24,15 @@ var common = {
         var name = $(this).attr("name");
         common.dom.navFocused = true;
         if(name=="index")
-          common.showMessage(122);
+          common.showMessage(122,"");
         else if(name=="post")
-          common.showMessage(154);
+          common.showMessage(154,"");
         else if(name=="project")
-          common.showMessage(148);
+          common.showMessage(148,"");
         else if(name=="fun")
-          common.showMessage(110);
+          common.showMessage(110,"");
         else if(name=="contact")
-          common.showMessage(138);
+          common.showMessage(138,"");
       });
       navLinks.mouseleave(function(){
         common.dom.navFocused = false;
@@ -38,37 +40,71 @@ var common = {
       
     },
     
-    showMessage : function(n)
+    prepareMessageInput : function(msgInput){
+        var msgInputField = msgInput.find('.input-wrapper input');
+        var msgInputSubmitButton = msgInput.find('.submit-button');
+        msgInputField.focus(function(){
+            if(msgInputField.val()=="Say something?")
+                msgInputField.val("");
+        });
+        msgInputField.blur(function(){
+            if(msgInputField.val()=="")
+                msgInputField.val("Say something?");
+        });
+        msgInputSubmitButton.click(function(){
+            
+                common.showMessage(1,"Fill in something then:(");
+        });
+    },
+    
+    showMessage : function(n, str)
     {
-      var msgBoxWrapper = common.dom.msgBoxWrapper;
-      if(msgBoxWrapper.hasClass('inDisplay'))
-      {
-        var original = msgBoxWrapper.find('.msg-box').html();
-        var noStr = n+'.png';
-        var originalFile = original.match(/[0-9]{1,3}.png/);
-        if(noStr!=originalFile)
+        var msgBoxWrapper = common.dom.msgBoxWrapper;
+      
+        if(msgBoxWrapper.hasClass('inDisplay'))
         {
-          /* already in display & not same, dim the msg box & show again with new pic */
-          msgBoxWrapper.stop().fadeOut(
-          { 
-            duration: 200,
-            complete: function(){
-              var htmlStr = '<img src="public/images/msg3/'+ n + '.png"/>';
-              msgBoxWrapper.find('.msg-box').html(htmlStr);
-              msgBoxWrapper.fadeIn();
+            var original = msgBoxWrapper.find('.msg-box').html();
+            var newStr="",oriStr="";
+            if(str=="")
+            {
+                newStr = newStr+n+'.png';
+                oriStr = oriStr+original.match(/[0-9]{1,3}.png/);
             }
-          });
+            else
+            {
+                oriStr = oriStr+original;
+                newStr = newStr+'<span>'+str+'</span>';
+            }
+            if(oriStr!=newStr)
+            {
+            /* already in display & not same, dim the msg box & show again with new pic */
+            msgBoxWrapper.stop().fadeOut(
+            { 
+                duration: 200,
+                complete: function(){
+                    if(str=="")
+                        var htmlStr = '<img src="public/images/msg3/'+ n + '.png"/>';
+                    else
+                        var htmlStr = newStr;
+                    msgBoxWrapper.find('.msg-box').html(htmlStr);
+                    msgBoxWrapper.fadeIn();
+                }
+            });
+            }
+        }else{
+            var htmlStr="";
+            if(str=="")
+                htmlStr = '<img src="public/images/msg3/'+ n + '.png"/>';
+            else
+                htmlStr = htmlStr+'<span>'+str+'</span>';
+            msgBoxWrapper.find('.msg-box').html(htmlStr);
+            msgBoxWrapper.addClass('inDisplay');
+            msgBoxWrapper.fadeIn(
+            {
+                duration: 200
+            }
+            );
         }
-      }else{
-        var htmlStr = '<img src="public/images/msg3/'+ n + '.png"/>';
-        msgBoxWrapper.find('.msg-box').html(htmlStr);
-        msgBoxWrapper.addClass('inDisplay');
-        msgBoxWrapper.fadeIn(
-          {
-            duration: 200
-          }
-        );
-      }
     },
     
     hideMessage : function()
@@ -95,7 +131,7 @@ var common = {
           /* if idle */
           if(common.dom.idleCounter>0)
           {
-            common.showMessage(rpic);
+            common.showMessage(rpic, "");
             common.dom.busyCounter = 0;
             common.dom.idleCounter = 0;
           }
@@ -116,4 +152,19 @@ var common = {
         common.dynamicMessage();
       }, rtime);
     }
+}
+
+function sendAjaxCall(url,type,data,callback){
+    $.ajax(
+    {
+        url: url,
+        cache: false,
+        dataType: 'json',
+        type: type,
+        data: data,
+        success: callback,
+        error: function(e, xhr)
+        {
+        }
+    });
 }
